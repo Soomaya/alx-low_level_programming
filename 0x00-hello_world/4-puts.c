4205d0b8224ebe3e00ae9b6ee68d73d7359b9670
Soomaya <soumayaelfadili@gmail.com>
2023-09-08 10:30:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+   *main - Entry point
+   *Discription: 'print number of base 10'
+   *Return: always 0
+   */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
