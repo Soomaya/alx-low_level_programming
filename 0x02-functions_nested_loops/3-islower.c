6c09f556e038a260a50ef8fe6b619417a82fd039
Soomaya <soumayaelfadili@gmail.com>
2023-09-12 11:46:34 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,15 +7,16 @@
   *
   * Return: 1 for lowercase character. 0 for the rest
   */
-int _islower(int c)
+int main(void)
 {
-	if (c >= 97 && c <= 122)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-	 _putchar('\n');
+	int r;
+
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
 }
